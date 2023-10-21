b1ff6a4bfffd43b28be3b5ab6a3fb0214bedf3a8
Lukasz Bien <lukaszbien90d@gmail.com>
2023-10-22 01:28:57 +0200

    Fixes, also for build version of project

--- server-rest-express/src/seeders/index.ts
+++ server-rest-express/src/seeders/index.ts
@@ -7,14 +7,16 @@ import { envConfig } from '@/config';
 const {
     postgres: { url, pool }
 } = envConfig;
-
 const sequelize = new Sequelize(url, {
     pool,
     models: [path.join(__dirname, '..', 'models')]
 });
+const fileExtension = path.extname(__filename);
 
 export const umzug = new Umzug({
-    migrations: { glob: 'src/seeders/!(*index).ts' },
+    migrations: {
+        glob: `${fileExtension === '.ts' ? 'src' : 'dist'}/seeders/!(*index).*`
+    },
     context: sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize }),
     logger: console
