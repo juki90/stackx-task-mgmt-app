1e50849cbf39918f73e04d9809d50f9520277865
Lukasz Bien <lukaszbien90d@gmail.com>
2023-12-06 15:57:09 +0100

    Changes in Zustand frontend

--- client-rest-react-zustand/src/pages/Dashboard.tsx
+++ client-rest-react-zustand/src/pages/Dashboard.tsx
@@ -2,35 +2,17 @@ import { Box, Divider, Typography } from '@mui/material';
 
 import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { MyDashboard } from '@/components/Auth/MyDashboard';
+import { PageTitleAndDescription } from '@/components/PageTitleAndDescription';
 
 import type { FC } from 'react';
 
 const DashboardPage: FC = () => (
     <ErrorBoundary>
         <Box height="100vh">
-            <Box
-                sx={{
-                    backgroundImage:
-                        'repeating-linear-gradient(45deg, #acf 0, #136 100px, #acf 200px);',
-                    backgroundClip: 'text',
-                    marginBottom: '20px'
-                }}
-            >
-                <Typography
-                    component="h1"
-                    variant="h3"
-                    sx={{
-                        color: 'transparent',
-                        fontWeight: 'bold',
-                        marginBottom: '10px'
-                    }}
-                >
-                    Dashboard
-                </Typography>
-                <Typography component="p" variant="h6">
-                    Your profile details and list of assigned tasks
-                </Typography>
-            </Box>
+            <PageTitleAndDescription
+                title="Dashboard"
+                description="Your profile details and list of assigned tasks"
+            />
             <Divider />
             <MyDashboard />
         </Box>
